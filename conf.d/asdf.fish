3a96959a10a66f84dd148c701ad3e5105e2378c2
David Mora <david.garcia@joincoup.com>
2022-04-28 09:37:30 +0200

    Add path for Brew 3 / Apple Silicon

--- conf.d/asdf.fish
+++ conf.d/asdf.fish
@@ -4,4 +4,6 @@ else if test -f ~/.asdf/asdf.fish
     source ~/.asdf/asdf.fish
 else if test -f /usr/local/opt/asdf/asdf.fish
     source /usr/local/opt/asdf/asdf.fish
+else if test -f /opt/homebrew/opt/asdf/asdf.fish
+    source /opt/homebrew/opt/asdf/asdf.fish
 end
