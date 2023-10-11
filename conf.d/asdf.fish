8d7aca3db7819ca404ee1452726b9749022cce2d
Vitaly Shishlyannikov <dark.dr4g0n@gmail.com>
2023-10-11 20:27:53 +0300

    Use asdf provided completions instead of imported

--- conf.d/asdf.fish
+++ conf.d/asdf.fish
@@ -9,3 +9,20 @@ else if test -f /opt/homebrew/opt/asdf/asdf.fish
 else if test -f /opt/homebrew/opt/asdf/libexec/asdf.fish
     source /opt/homebrew/opt/asdf/libexec/asdf.fish
 end
+
+# Link completions in git-based setup
+function _asdf_install --on-event asdf_install
+    if test -f ~/.asdf/asdf.fish
+        and not test -L ~/.config/fish/completions/asdf.fish
+            if not test -d ~/.config/fish/completions
+                mkdir -p ~/.config/fish/completions
+            end
+            ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions/asdf.fish
+    end
+end
+
+function _asdf_uninstall --on-event asdf_uninstall
+    test -f ~/.asdf/asdf.fish
+        and test -L ~/.config/fish/completions/asdf.fish
+            rm ~/.config/fish/completions/asdf.fish
+end
